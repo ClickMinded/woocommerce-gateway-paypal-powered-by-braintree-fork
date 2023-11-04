b95ba383bb84e4a267677b13f4afd660b171e1ba
Eddy Valencia <eddyvalencia8@gmail.com>
2023-11-04 17:26:25 -0500

    Added: dispatch event error on refresh_braintree function

--- class-wc-braintree.php
+++ class-wc-braintree.php
@@ -36,7 +36,7 @@ class WC_Braintree extends Framework\SV_WC_Payment_Gateway_Plugin {
 
 
 	/** plugin version number */
-	const VERSION = '3.0.6.8'; // WRCS: DEFINED_VERSION.
+	const VERSION = '3.0.6.9'; // WRCS: DEFINED_VERSION.
 
 	/** Braintree JS SDK version  */
 	const BRAINTREE_JS_SDK_VERSION = '3.94.0';
